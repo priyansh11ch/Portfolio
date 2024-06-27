9e9fce16acd4300f4fe3589a825747229491b474
Priyansh Chaudhary <154365308+priyansh11ch@users.noreply.github.com>
2024-06-27 18:42:15 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+function download(){
+    window.location = "https://drive.google.com/file/d/1BXqmYs0c9AXveIrt9AVH7b767qSNX84v/view"
+}
+
+function linkedIN(){
+    window.location = "www.linkedin.com/in/priyansh-chaudhary-6a58a5221"
+}
